57a58dec22e265a312cac81bed28906cc0f29a0f
soroosh <sorooshm78@gmail.com>
2022-07-09 07:50:25 -0400

    add modal to del-btn => fix Error

--- static/modal/modal.js
+++ static/modal/modal.js
@@ -1,9 +1,11 @@
-function delBtnOnClick() {
+function delBtnOnClick(url) {
+    $('#url').val(url);
     var modal = $('#delModal');
     modal.style.display = "block";
 }
 
-function doDelete(url) {
+function doDelete() {
+    var url = $('#url').val();
     $.get(url);
     location.reload();
 }
