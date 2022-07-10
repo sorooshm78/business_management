521da206b176fc8ecc875c514caead1e6fbd59cc
soroosh <sorooshm78@gmail.com>
2022-07-10 07:14:44 -0400

    add log to backup

--- backup/backup.py
+++ backup/backup.py
@@ -3,14 +3,15 @@ import os
 
 # setting config
 import shutil
-import time
 from pathlib import Path
 
 PATH_BACKUP_FILE = '../db.sqlite3'
 PATH_BACKUP_FOLDER = './saved'
-ROTATE = 3
+ROTATE = 30
+
+
 # second
-TIME = 60
+# TIME = 60
 
 
 def create_backup():
@@ -19,29 +20,32 @@ def create_backup():
     os.mkdir(mk_path)
     # copy backup
     shutil.copy(PATH_BACKUP_FILE, mk_path)
+    logfile.write(f'add dir : {mk_path}\n')
 
 
 if __name__ == "__main__":
-    while True:
-        # check exist backup file
-        if not (os.path.exists(PATH_BACKUP_FILE)):
-            raise "BACKUP FILE IS NOT EXIST"
+    logfile = open('log.txt', 'a')
 
-        # check exist backup folder if not created
-        Path(PATH_BACKUP_FOLDER).mkdir(parents=True, exist_ok=True)
+    # check exist backup file
+    if not (os.path.exists(PATH_BACKUP_FILE)):
+        raise "BACKUP FILE IS NOT EXIST"
 
-        backup_folders = sorted(os.listdir(PATH_BACKUP_FOLDER))
-        len_backup_folders = len(backup_folders)
+    # check exist backup folder if not created
+    Path(PATH_BACKUP_FOLDER).mkdir(parents=True, exist_ok=True)
 
-        if len_backup_folders >= ROTATE:
-            # del folders
-            for index in range(len_backup_folders - ROTATE + 1):
-                del_path = os.path.join(PATH_BACKUP_FOLDER, backup_folders[index])
-                # os.rmdir(del_path)
-                shutil.rmtree(del_path)
+    backup_folders = sorted(os.listdir(PATH_BACKUP_FOLDER))
+    len_backup_folders = len(backup_folders)
 
-            create_backup()
-        else:
-            create_backup()
+    if len_backup_folders >= ROTATE:
+        # del folders
+        for index in range(len_backup_folders - ROTATE + 1):
+            del_path = os.path.join(PATH_BACKUP_FOLDER, backup_folders[index])
+            # os.rmdir(del_path)
+            shutil.rmtree(del_path)
+            logfile.write(f'del dir : {del_path}\n')
 
-        time.sleep(TIME)
+        create_backup()
+    else:
+        create_backup()
+
+    logfile.close()
